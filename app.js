d43ed7d901dd9ad8281a61cedb852b56ddba8991
RJ Shoemaker <rjshoemaker55@gmail.com>
2022-01-25 08:15:27 -0500

    functionality fixes

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
-let myLibrary = [
-  // {
-  //   title: 'The Hobbit',
-  //   author: 'J.R.R. Tolkien',
-  //   pages: 234,
-  //   read: true
-  // },
-  // {
-  //   title: 'Hunger Games',
-  //   author: 'Suzanna Collins',
-  //   pages: 150,
-  //   read: false
-  // }
-]
+let myLibrary = []
 
+// Book constructor
 function Book(title, author, pages) {
   this.title = title
   this.author = author
@@ -20,7 +8,9 @@ function Book(title, author, pages) {
   this.read = false
 }
 
+// Add book to library
 function addBookToLibrary(book) {
+  console.log('add book to libr')
   const newBook = document.createElement('div')
   newBook.classList.add('book-card')
 
@@ -41,6 +31,7 @@ function addBookToLibrary(book) {
     setRead(book, newBookRead)
   })
 
+  // Delete book from library
   const newBookDelete = document.createElement('button')
   newBookDelete.innerHTML = 'Delete'
   newBookDelete.addEventListener('click', () => {
@@ -59,8 +50,10 @@ function addBookToLibrary(book) {
   document.getElementById('books-wrapper').append(newBook)
 }
 
+// Book form logic
 const addBookForm = document.getElementById('add-book-form')
 addBookForm.addEventListener('submit', (event) => {
+  console.log('submited form')
   event.preventDefault()
 
   const bookToAdd = new Book(
@@ -71,24 +64,19 @@ addBookForm.addEventListener('submit', (event) => {
 
   addBookToLibrary(bookToAdd)
   myLibrary.push(bookToAdd)
-
-  event.target.elements[0].value = ''
-  event.target.elements[1].value = ''
-  event.target.elements[2].value = ''
+  clearForm(addBookForm)
+  hide(addBookForm)
 })
 
+// Update books in library
 function updateBooks() {
   if (myLibrary) {
     document.getElementById('books-wrapper').innerHTML = ''
-
     myLibrary.forEach((book) => addBookToLibrary(book))
   }
 }
 
-function setRead(book, readButton) {
-  readButton.innerText = book.read ? 'Read' : 'Not Read'
-}
-
+// Trigger form to show
 const addBookButton = document.getElementById('add-book-button')
 addBookButton.addEventListener('click', (button) => {
   if (addBookForm.dataset.show === 'true') {
@@ -100,11 +88,15 @@ addBookButton.addEventListener('click', (button) => {
   }
 })
 
+// Close book form
 const addBookFormClose = document.getElementById('add-book-form__close')
-addBookFormClose.addEventListener('click', () => {
+addBookFormClose.addEventListener('click', (event) => {
+  event.preventDefault()
+  clearForm(addBookForm)
   hide(addBookForm)
 })
 
+// Helper functions
 function hide(element) {
   element.classList.remove('show')
   element.classList.add('hide')
@@ -113,6 +105,24 @@ function hide(element) {
 function show(element) {
   element.classList.remove('hide')
   element.classList.add('show')
+}
+
+function setRead(book, readButton) {
+  readButton.innerText = book.read ? 'Read' : 'Not Read'
+}
+
+function clearForm(form) {
+  const formInputElements = form.children
+
+  for (i = 0; i < formInputElements.length; i++) {
+    if (
+      formInputElements[i].nodeName === 'INPUT' &&
+      (formInputElements[i].type === 'text' ||
+        formInputElements[i].type === 'number')
+    ) {
+      formInputElements[i].value = ''
+    }
+  }
 }
 
 updateBooks()
